b63fe9404fff1e8cb57e27334faadbbfd87da981
Dmitrij Larin <larin4921@gmail.com>
2024-11-23 20:33:03 +0300

    Указали абсолютный путь к файлу test_file_3.txt с помощью функции os.path.join()

--- Module 6_1/task_6_2_1/main.py
+++ Module 6_1/task_6_2_1/main.py
@@ -6,11 +6,27 @@ import os
 # print()
 
 # Выведем содержимое папки task_6_2_1
-path_learning = r"C:\Users\dmitr\OneDrive\Documentos\Обучение на Python\Module 6. Homework\Module 6_1\task_6_2_1"
-for path, dirnames, filenames in os.walk(path_learning):
-    print(f"path - {path}")
-    print(f"dirnames - {dirnames}")
-    print(f"filenames - {filenames}")
+# path_learning = r"C:\Users\dmitr\OneDrive\Documentos\Обучение на Python\Module 6. Homework\Module 6_1\task_6_2_1"
+# for path, dirnames, filenames in os.walk(path_learning):
+#     print(f"path - {path}")
+#     print(f"dirnames - {dirnames}")
+#     print(f"filenames - {filenames}")
+# print()
+
+# Укажем абсолютный путь к файлу test_file_3.txt с помощью функции os.path.join()
+disk = 'C:\\'
+dir_1 = 'Users'
+dir_2 = 'dmitr'
+dir_3 = 'OneDrive'
+dir_4 = 'Documentos'
+dir_5 = 'Обучение на Python'
+dir_6 = 'Module 6. Homework'
+dir_7 = 'Module 6_1'
+dir_8 = 'task_6_2_1'
+dir_9 = 'data_path_2'
+file = 'test_file_3.txt'
+path_test_file_3 = os.path.join(disk, dir_1, dir_2, dir_3, dir_4, dir_5, dir_6, dir_7, dir_8, dir_9, file)
+print(path_test_file_3)
 
 
 
