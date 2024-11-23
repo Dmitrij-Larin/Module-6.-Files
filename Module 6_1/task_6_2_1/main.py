c6918a1ef442e4aaf3c573edecd5d046f412c521
Dmitrij Larin <larin4921@gmail.com>
2024-11-23 20:13:59 +0300

    Вывели содержимое папки task_6_2_1

--- Module 6_1/task_6_2_1/main.py
+++ Module 6_1/task_6_2_1/main.py
@@ -1,6 +1,16 @@
 import os
 
 # Укажем в консоли абсолютный путь к файлу test_file_1.txt
-my_abspath = os.path.abspath(r'.\data_path_1\test_file_1.txt')
-print(my_abspath)
+# my_abspath = os.path.abspath(r'.\data_path_1\test_file_1.txt')
+# print(my_abspath)
+# print()
+
+# Выведем содержимое папки task_6_2_1
+path_learning = r"C:\Users\dmitr\OneDrive\Documentos\Обучение на Python\Module 6. Homework\Module 6_1\task_6_2_1"
+for path, dirnames, filenames in os.walk(path_learning):
+    print(f"path - {path}")
+    print(f"dirnames - {dirnames}")
+    print(f"filenames - {filenames}")
+
+
 
